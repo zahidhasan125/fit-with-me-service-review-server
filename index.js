631d4606471526c1421df2f7ccc2ff984df0ebd1
zahidhasan125 <zahidhasan125@gmail.com>
2022-11-08 13:02:59 +0600

    dynamic service api created

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
@@ -8,10 +9,7 @@ require('dotenv').config()
 app.use(cors());
 app.use(express.json());
 
-// fitnessUserdb:3vavz8IXRXJpA0ZE
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.voxvdqi.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -24,6 +22,19 @@ async function run() {
             const query = {};
             const services = await serviceCollection.find(query).limit(3).toArray();
             res.send(services);
+
+        })
+        app.get('/allservices', async(req, res) => {
+            const query = {};
+            const allServices = await serviceCollection.find(query).toArray();
+            res.send(allServices);
+
+        })
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
 
         })
 
